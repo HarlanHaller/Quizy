1ced642f49bb166229bc6459fcf8a1bda47ca789
Harlan Haller <harlanhaller06@gmail.com>
2022-12-06 17:37:16 -0500

    implemented flashcards mode and style changes throughout

---
+++ public/js/flashcards.js
@@ -0,0 +1,125 @@
+const userdataPromise = window.dataLoader.getUserData();
+const setName = window.location.search.split('?set=')[1];
+var termIndex = 0;
+var set, index, termList = [];
+
+const rightArrow = (e, duration) => {
+    termIndex++;
+    renderTerm();
+    $('card').animate(
+        [
+            {
+                transform: 'translateX(200px)',
+                opacity: 0
+            },
+            {
+                transform: 'translateX(0)',
+                opacity: 1
+            }
+        ],
+        {
+            duration: duration,
+            iterations: 1,
+            easing: 'ease-out'
+        }
+    );
+};
+
+const leftArrow = (e, duration) => {
+    termIndex--;
+    renderTerm();
+    $('card').animate(
+        [
+            {
+                transform: 'translateX(-200px)',
+                opacity: 0
+            },
+            {
+                transform: 'translateX(0)',
+                opacity: 1
+            }
+        ],
+        {
+            duration: duration,
+            iterations: 1,
+            easing: 'ease-out'
+        }
+    );
+};
+
+const flipCard = (e) => {
+    let badTargets = ['leftArrow', 'rightArrow', 'gg-arrow-long-right', 'gg-arrow-long-left'];
+    if (!badTargets.includes(e.target.className)) {
+        $("card").className == 'flip-card animations'
+            ? ($("card").className = 'flip-card animations flipped')
+            : ($("card").className = 'flip-card animations');
+    }
+}
+
+$('card').addEventListener('click', function (e) { flipCard(e) });
+
+document.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'ArrowLeft':
+            leftArrow(e, 100);
+            break;
+        case 'ArrowRight':
+            rightArrow(e, 100);
+            break;
+        case ' ':
+            flipCard(e);
+            break;
+        default:
+            break;
+    }
+});
+
+
+Array.from($class('rightArrow')).forEach((e) => e.addEventListener('click', () => { rightArrow(e, 300) }));
+Array.from($class('leftArrow')).forEach((e) => e.addEventListener('click', () => { leftArrow(e, 300) }));
+
+function $(id) {
+    return document.getElementById(id);
+}
+
+function $class(className) {
+    return document.getElementsByClassName(className);
+}
+
+function getSet(userdata, setName) {
+    for (let i of userdata.sets) {
+        if (i.metadata.name == setName) return i;
+    }
+    throw 'not a set';
+}
+
+function genTermArr(kSet) {
+    let temp = [];
+    for (part in kSet.cards) {
+        for (item in kSet.cards[part]) {
+            temp.push([item, kSet.cards[part][item]]);
+        }
+    }
+    return temp;
+}
+
+function renderTerm() {
+    termIndex = termIndex >= 0 ? (termIndex >= termList.length ? 0 : termIndex) : termList.length - 1;
+    $('term').innerText = termList[termIndex][0];
+    $('definition').innerText = termList[termIndex][1];
+}
+
+userdataPromise.then((userdata) => {
+    try {
+        set = getSet(userdata, setName);
+    } catch (error) {
+        document.body.innerHTML = 'no set found';
+        console.error(error);
+        return;
+    }
+    index = userdata.sets.findIndex((e) => {
+        return e === set;
+    });
+    termList = genTermArr(set);
+    renderTerm();
+});
--- public/js/sets.js
+++ public/js/sets.js
@@ -2,10 +2,17 @@ const userdataPromise = window.dataLoader.getUserData();
 const setName = window.location.search.split("?set=")[1];
 var set, index;
 
-document.getElementById("sortOptions").addEventListener("change", function () {
+function $(id) {
+    return document.getElementById(id)
+}
+
+$("sortOptions").addEventListener("change", function () {
     console.log(this.value);
     //TODO implement alternate sorting methods
 })
+
+$("Flashcards").addEventListener("click", () => link(`./flashcards.html?set=${setName}`));
+// $("Learn").addEventListener("click", link(`./learn.html?set=${setName}`));
 
 function objLen(obj) {
     return Object.keys(obj).length;
@@ -31,10 +38,14 @@ function getSet(userdata, setName) {
 
 function generateTermList(terms, definitions) {
     for (let i = 0; i < terms.length; i++) {
-        let row = createElem("div", document.getElementById("termList"), "", { "class": "term" });
+        let row = createElem("div", $("termList"), "", { "class": "term" });
         createElem("p", createElem("div", createElem("div", row)), terms[i]);
         createElem("p", createElem("div", createElem("div", row)), definitions[i])
     }
+}
+
+function link(url) {
+    window.location.assign(url);
 }
 
 userdataPromise.then((userdata) => {
@@ -46,7 +57,7 @@ userdataPromise.then((userdata) => {
         return;
     }
     index = userdata.sets.findIndex((e) => { return e === set })
-    let termList = document.getElementById("termList")
+    let termList = $("termList")
     if (objLen(set.cards.notStudied) > 0) {
         createElem("h4", createElem("div", termList, "", { "class": "subheader" }), `Not Studied (${objLen(set.cards.notStudied)})`);
         generateTermList(Object.keys(set.cards.notStudied), Object.values(set.cards.notStudied));
