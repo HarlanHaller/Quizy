47244dfcfdf1c2e7da61833fcda070504533a056
Harlan Haller <harlanhaller06@gmail.com>
2023-05-12 17:28:54 -0400

    small changes in preparation. changed gitignore

--- preload.js
+++ preload.js
@@ -1,21 +1,22 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-const { contextBridge, ipcRenderer } = require('electron')
+const { contextBridge, ipcRenderer } = require("electron");
 
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
-    if (element) element.innerText = text;
-  }
+window.addEventListener("DOMContentLoaded", () => {
+    const replaceText = (selector, text) => {
+        const element = document.getElementById(selector);
+        if (element) element.innerText = text;
+    };
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
-})
+    for (const type of ["chrome", "node", "electron"]) {
+        // eslint-disable-next-line no-undef
+        replaceText(`${type}-version`, process.versions[type]);
+    }
+});
 
 contextBridge.exposeInMainWorld("dataLoader", {
-  getUserData: () => ipcRenderer.invoke('request-userdata'),
-  setLastOpened: (setName) => {
-    ipcRenderer.send('last-opened', setName);
-  }
+    getUserData: () => ipcRenderer.invoke("request-userdata"),
+    setLastOpened: (setName) => {
+        ipcRenderer.send("last-opened", setName);
+    }
 });
